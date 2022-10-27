7ccfab0e22e36da3280274eaaec751906f74c256
jackuna <kunalkantisaha@gmail.com>
2022-10-27 14:36:47 +0530

    Data discrepency fixed #2 for lambda app

--- sam-lambda-app/webdigger/app.py
+++ sam-lambda-app/webdigger/app.py
@@ -25,8 +25,6 @@ def lambda_handler(event, context):
         No argument required for now.
         '''
 
-        global driver
-
         # ----------------------------------web driver setup---------------------------------------------------
         # path to chromedriver. while testing locally, 'var/task/' is the temporary path where files and dependencies of the function are mounted.
         # 'var/task/' path stays the same for files and dependencies in aws lambda environment, when you upload the function as a zip file.
@@ -76,13 +74,11 @@ def lambda_handler(event, context):
         global url_dict_data
         global data_dir
         global today
-        global s3_upload_mock
 
         dt = datetime.now()
         today = dt.strftime('%m%d%y')
         print(len(event.keys()))
         data_dir="/tmp/data/"
-        s3_upload_mock = True
 
         os.mkdir(data_dir)
 
@@ -121,17 +117,19 @@ def lambda_handler(event, context):
     # ---------------------------------- upload_file_to_s3 function ends here ---------------------------------------
 
 
-    def yt_channel_scrapper(channel_url, scrap_post_limit, s3_mock_status):
+    def yt_channel_scrapper(channel_urls, scrap_posts_count_limit, s3_mock_status, scrap_post_day_age ):
+        
+        driver = initialize_driver()
 
         '''
         Scrap the youtube channel video posts titles and uploads it into s3 bucket.
         channel_url = dictionary value of channel by channel name.
         '''
         
-        for val in channel_url.keys():
+        for val in channel_urls.keys():
 
-            youtube_page = channel_url[val]['link']
-            channel_name = channel_url[val]['link'].split('/')[4].lower()
+            youtube_page = channel_urls[val]['link']
+            channel_name = channel_urls[val]['link'].split('/')[4].lower()
             file_name = "report_{}_{}_data.csv".format(today,channel_name)
             file_name_with_dir = "{}{}".format(data_dir,file_name)
 
@@ -147,45 +145,49 @@ def lambda_handler(event, context):
             time.sleep(2)
 
             print("\n Scrapting data for :", youtube_page,"\n")
-            video_posted_in_48h=0
+            
+            video_posted_in_requested_age=0
 
-            while video_posted_in_48h >= 0:
+            while video_posted_in_requested_age >= 0:
                 driver.execute_script("window.scrollTo(0,Math.max(document.documentElement.scrollHeight,document.body.scrollHeight,document.documentElement.clientHeight))")
                 time.sleep(1)
-                try:
-                    video_posted_in_24h=len(driver.find_elements(By.XPATH,'//*[contains(text(),"hour")]'))
 
-                except:
-                    video_posted_in_24h=0
+                construct_xpath_string_start = '//*[contains(text(),"{} day")]'.format(scrap_post_day_age)
+                construct_xpath_string_end = '//*[contains(text(),"{} day")]'.format(scrap_post_day_age+1)
+
                 try:
-                    video_posted_in_48h=len(driver.find_elements(By.XPATH,'//*[contains(text(),"1 day")]'))
+                    video_posted_in_requested_age=len(driver.find_elements(By.XPATH,construct_xpath_string_start))
                 except:
-                    video_posted_in_48h=0
-                
-                if video_posted_in_48h >= scrap_post_limit:
-                    print ("Scrap daata limit reached, set to write data for post count", scrap_post_limit)
-                    break
-                    
-                if len(driver.find_elements(By.XPATH,'//*[contains(text(),"2 day")]')) > 0:
+                    video_posted_in_requested_age=0
+            
+                if len(driver.find_elements(By.XPATH,construct_xpath_string_end)) > 0:
                     break
 
-            print("Total Videos in last 48 hours :", video_posted_in_48h)
+
+            total_post_to_write = len(driver.find_elements(By.ID, 'video-title'))
+            print("Total video posted by {} day ago : {} \n".format(scrap_post_day_age, total_post_to_write))
 
             yt_title=[]
             yt_views=[]
             yt_posted=[]
             youtube_dict = {}
+            new_count = 0
 
-            for count in range(0,video_posted_in_48h):
+            for count in range(0,total_post_to_write):
+            
                 video_title=driver.find_elements(By.ID, 'video-title')[count].text
-                video_views=driver.find_elements(By.XPATH, '//*[@id="metadata-line"]/span[1]')[count].text.rstrip('views')
-                video_posted=driver.find_elements(By.XPATH, '//*[@id="metadata-line"]/span[2]')[count].text
-                #time.sleep(1)
-                yt_title.append(video_views)
+                video_views=driver.find_elements(By.XPATH, '//*[@id="metadata-line"]/span[1]')[count].text
 
                 youtube_dict['yt_title'] = video_title
                 youtube_dict['yt_views'] = video_views
-                youtube_dict['yt_posted'] = video_posted
+
+                if video_views.split()[1] == "watching":
+                    youtube_dict['yt_posted'] = "Stream ongoing.."
+                    new_count = count - 1 
+                else:
+                    video_posted=driver.find_elements(By.XPATH, '//*[@id="metadata-line"]/span[2]')[new_count].text
+                    youtube_dict['yt_posted'] = video_posted
+
 
                 print(video_title, video_views, video_posted)
                 writer.writerow(youtube_dict.values())
@@ -209,9 +211,14 @@ def lambda_handler(event, context):
             print(file)
 
     
-    initiliaze_data("channel_list.json")
-    initialize_driver()
-    yt_channel_scrapper(url_dict_data, 101, False)
+
+    def main():
+        initiliaze_data("channel_list.json")
+        yt_channel_scrapper(url_dict_data, 101, True, 1)
+
+    
+    main()
+
     #print_data()
     return { 
         "status": 200,
